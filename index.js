67e09e8b10a3181c6eed64b5aec2c7f855d66207
night-b <joshbb412@gmail.com>
2026-09-05 13:01:21 +0100

    test

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express")
 const mongoose = require('mongoose');
 const userRoute = require("./routes/userRoutes.js");
 
-// const compass_str = "mongodb://localhost:27017/TODO"
-const atlas_string = "mongodb://joshbb412_db_user:ihopeitworks@cluster0.nvtytwk.mongodb.net/?appName=Cluster0"
+const compass_str = "mongodb://localhost:27017/"
+// const atlas_string = "mongodb+srv://joshbb412_db_user:ihopeitworks@cluster0.nvtytwk.mongodb.net/?appName=Cluster0"
 
-mongoose.connect(atlas_string)
+mongoose.connect(compass_str)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.error("Connection Error: ", err));
 
